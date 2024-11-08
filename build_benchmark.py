54b9b9d6b07eeb3c3da798d26b7be6fdcadc19a5
Zakary Nafziger <zsnafzig@ece.ubc.ca>
2024-11-08 12:08:16 -0800

    major refactor of build_benchmark
    encapsulate everything in a class, and rename things to agree with the
    paper

--- build_benchmark.py
+++ build_benchmark.py
@@ -5,183 +5,214 @@ import argparse
 import shutil
 import configparser
 
-def clone_source_repo(benchmark):
+class ChronbenchBenchmark:
     '''
-    Clone the source repo, and reset it to the base commit. The `benchmark`
-    object includes the url and base commit.
-
-    We clone instead of using git submodules, since submodules produce a
-    complicated .git/ structure that confuses git-filter-repo.
+    Manipulate Chronbench Benchmarks.
     '''
+    def __init__(self, benchmark_desc_file, relative_gfr_path):
+        '''
+        Initialize benchmark state
+        '''
+        self.gfr_path = relative_gfr_path
 
-    name = benchmark.sections()[0]
-    repo_url = benchmark[name]['url']
+        benchmark_desc = self._parse_benchmark_desc_file(benchmark_desc_file)
+        self.name = benchmark_desc.sections()[0]
+        self.benchmark = benchmark_desc[self.name]
 
-    # bailout if the repo all ready exists
-    isdir = os.path.isdir(name)
-    if isdir:
-        print("QUITTING: "+name+" already exists!")
-        print("          perhaps re-run with `--clean`?")
-        exit()
+        # create variables for all the mandatory benchmark description fields
+        self.repo_url = self.benchmark['url']
+        self.base_sha = self.benchmark['start']
+        self.branch   = self.benchmark['branch']
+        self.depth    = self.benchmark['depth']
+        self.fileset  = self.benchmark['fileset'].split()
+        self.squash_list = self.benchmark['squash-list'].split()
 
-    # clone the source repo
-    subprocess.run(['git', 'clone', repo_url])
+        self.phony_author = 'Chronbench <chronbench@email.com>'
 
-def reset_source_repo_head(benchmark):
-    '''
-    Set the HEAD of the source repo to the start commit
-    '''
-    name = benchmark.sections()[0]
-    base_sha = benchmark[name]['start']
+    def build_benchmark(self):
+        '''
+        Create a fresh benchmark. Result is an incremental benchmark repository.
+        '''
 
-    # reset the source repo to the base commit
-    subprocess.run(['git', 'reset', '--hard', base_sha], cwd=name)
+        # Step 0 - Get a fresh copy of the upstream repo, and reset it to a
+        # known base commit
+        self._clone_upstream_repository()
+        self._run_cmd('git reset --hard '+self.base_sha)
 
-def domesticate_source_repo(benchmark, relative_gfr_path):
-    '''
-    Construct flattening instructions from the fileset and then use the fileset
-    and the flattening instructions to rewrite the source repo.
+        # Step 1 - Reduce the upstream repo to the Fileset of Interest
+        self._reduce_to_fileset_of_interest()
 
-    result is a "domesticated" (i.e. suitable as a benchmark) repository
-    '''
+        # Step 2 - Reduce upstream repo to the Window of Interest
+        self._reduce_to_window_of_interest()
 
-    sys.path.insert(1, relative_gfr_path)
-    import git_filter_repo as gfr
+        # Step 3 - Squash unsynthesizable commits
+        self._squash_unsynthesizable_commits()
 
-    name = benchmark.sections()[0]
-    fileset = benchmark[name]['fileset'].split()
-    branch = benchmark[name]['branch']
+    def cleanup_benchmark(self):
+        '''
+        Delete a benchmark repository so that it can be rebuilt from scratch.
+        '''
 
-    # descend into the source repository
-    cwd = os.getcwd()
-    os.chdir(name)
+        isdir = os.path.isdir(self.name)
+        if not isdir:
+            print("QUITTING: "+self.name+" does not exist!")
+            exit()
 
-    # blow away everything that is not in the synthesizable fileset and flatten
-    # the directory structure
-    arg_list = ['--force', '--refs', branch]
-    for f in fileset:
-        arg_list.append('--path-match')
-        arg_list.append(f)
-        arg_list.append('--path-rename')
-        arg_list.append(f+':'+os.path.basename(f))
+        shutil.rmtree(self.name)
 
-    args = gfr.FilteringOptions.parse_args(arg_list)
-    filter = gfr.RepoFilter(args)
-    filter.run()
+    def _parse_benchmark_desc_file(self, benchmark_desc_file):
+        '''
+        Read a benchmark description file. Return a populated config object.
+        '''
+        config = configparser.ConfigParser()
+        config.read(benchmark_desc_file)
+        return config
 
-    # return to the original working directory
-    os.chdir(cwd)
+    def _run_cmd(self, cmd):
+        '''
+        Run a shell command in the benchmark directory. If cmd is a string
+        split it on spaces. If it is a list leave it as is. Strings are easier
+        to read, but lists allow for some command line fields to include spaces.
 
-def truncate_filtered_repo(benchmark):
-    '''
-    Create a new root commit equal to HEAD~<depth> and then rebase
-    HEAD~<depth>..HEAD onto the new root. We use <depth> rather than a SHA since
-    gfr produces new commits with unknown hashes.
-    '''
-    name = benchmark.sections()[0]
-    branch = benchmark[name]['branch']
-    new_branch = branch + "_new"
-    start_sha = benchmark[name]['start']
-    depth = benchmark[name]['depth']
+        Returns stdout as a list of utf8 strings.
+        '''
+        if type(cmd) is not list:
+            cmd = cmd.split()
+        result = subprocess.run(cmd, cwd=self.name, capture_output=True)
+        result = result.stdout.decode('utf8').split('\n')
+        return result
 
-    # HEAD~<depth> is HEAD + depth commits, so correct for ObO
-    depth = str(int(depth) - 1)
+    def _clone_upstream_repository(self):
+        '''
+        Clone the upstream repository
 
-    # get the stop SHA
-    stop_sha = subprocess.run(['git', 'show', '-s', 'HEAD~' + depth, '--format=format:%H'], cwd=name, capture_output=True)
-    stop_sha = stop_sha.stdout.decode('utf8')
+        We clone instead of using git submodules, since submodules produce a
+        complicated .git/ structure that confuses git-filter-repo.
+        '''
 
-    # create an orphan branch based on the stop commit
-    subprocess.run(['git', 'checkout', '--orphan', new_branch, stop_sha], cwd=name)
-    subprocess.run(['git', 'commit', '--author', 'Chronbench <chronbench@email.com>', '-m', 'new root'], cwd=name)
+        # bailout if the repo all ready exists
+        isdir = os.path.isdir(self.name)
+        if isdir:
+            print("QUITTING: "+self.name+" already exists!")
+            print("          perhaps re-run with `--clean`?")
+            exit()
 
-    # rebase the window of interest onto branch using the orphaned new_branch
-    # as a base
-    subprocess.run(['git', 'rebase', '--onto', new_branch, stop_sha, branch], cwd=name)
+        # clone the repo
+        subprocess.run(['git', 'clone', self.repo_url])
 
-    # get rid of the orphan branch
-    subprocess.run(['git', 'branch', '-D', new_branch], cwd=name)
+    def _reduce_to_fileset_of_interest(self):
+        '''
+        Construct flattening instructions from the fileset and then use the
+        fileset and the flattening instructions as input to git-filter-repo to
+        rewrite the source repo.
 
-def squash_uninteresting_commits(benchmark):
-    '''
-    Assuming that the benchmark repository has been distilled to the Fileset of
-    Interest and Window of interest, squash the commits in the squash-list, into
-    their oldest non-squash list successor. The squash list is formulated as a
-    list of natural numbers indicating the index of commits to squash. Must use
-    indices instead of hashes since gfr rewrites hashes. For the purposes of
-    the squash-list we assume branch is index 0.
-    '''
-    name = benchmark.sections()[0]
-    branch = benchmark[name]['branch']
-    new_branch = branch + '_new'
-    squash_list = benchmark[name]['squash-list'].split()
-    squash_shas = []
+        Result is a hierarchically flat repository (no directories) including
+        only commits that modify the Fileset of Interest.
+        '''
 
-    # enusre we are starting at index 0
-    subprocess.run(['git', 'checkout', branch], cwd=name)
+        # import git-filter-repo
+        sys.path.insert(1, self.gfr_path)
+        import git_filter_repo as gfr
 
-    # get the hash of each commit in the squash list
-    for s in squash_list:
-        sha = subprocess.run(['git', 'show', '-s', 'HEAD~'+s, '--format=format:%H'], cwd=name, capture_output=True)
-        sha = sha.stdout.decode('utf8')
+        # descend into the source repository
+        cwd = os.getcwd()
+        os.chdir(self.name)
 
-        squash_shas.append(sha)
+        # blow away everything that is not in the synthesizable fileset and
+        # flatten the directory structure
+        arg_list = ['--force', '--refs', self.branch]
+        for f in self.fileset:
+            arg_list.append('--path-match')
+            arg_list.append(f)
+            arg_list.append('--path-rename')
+            arg_list.append(f+':'+os.path.basename(f))
 
-    # get hashes for all of the interesting commits
-    unsquashed = subprocess.run(['git', 'log', '--format=format:%H'], cwd=name, capture_output=True)
-    unsquashed = unsquashed.stdout.decode('utf8').split('\n')
-    unsquashed.reverse()
+        args = gfr.FilteringOptions.parse_args(arg_list)
+        filter = gfr.RepoFilter(args)
+        filter.run()
 
-    # create a new branch based off the root commit
-    subprocess.run(['git', 'checkout', unsquashed[0]], cwd=name)
-    subprocess.run(['git', 'checkout', '-b', new_branch], cwd=name)
+        # return to the original working directory
+        os.chdir(cwd)
 
-    # cherry pick each commit from the original branch onto the new branch
-    # if a commit is in the squash list, squash it into the subsequent commit
-    to_squash = False
-    for cidx in range(1, len(unsquashed)):
-        subprocess.run(['git', 'cherry-pick', unsquashed[cidx]], cwd=name)
+    def _reduce_to_window_of_interest(self):
+        '''
+        Create a new root commit equal to HEAD~<depth> and then rebase
+        HEAD~<depth>..HEAD onto the new root. We use <depth> rather than a hash
+        since gfr produces new commits with unknown hashes.
 
-        # check to see if the last commit needs to be squashed into the one just
-        # applied
-        if to_squash:
-            # squash the last two commits, and accept the default message
-            subprocess.run(['git', 'reset', '--hard', 'HEAD~2'], cwd=name)
-            subprocess.run(['git', 'merge', '--squash', 'HEAD@{1}'], cwd=name)
-            subprocess.run(['git', 'commit', '--no-edit', '--author', 'Chronbench <chronbench@email.com>'], cwd=name)
+        Result is a repository with a straightline history including only
+        interesting commits.
+        '''
+        new_branch = self.branch + '_new'
 
-        # check if the commit we just applied needs to be squashed
-        if unsquashed[cidx] in squash_shas:
-            to_squash = True
-        else:
-            to_squash = False
+        # HEAD~<depth> is HEAD + depth commits, so correct for ObO
+        depth = str(int(self.depth) - 1)
 
-    # delete the unsquashed branch and rename the squashed branch
-    subprocess.run(['git', 'branch', '-D', branch], cwd=name)
-    subprocess.run(['git', 'branch', '-m', new_branch, branch], cwd=name)
+        # get the stop hash
+        stop_sha = self._run_cmd('git show -s HEAD~'+depth+' --format=format:%H')[0]
 
-def cleanup_benchmark(benchmark):
-    '''
-    delete a benchmark repository, so that it can be rebuilt from scratch.
-    '''
+        # create an orphan branch based on the stop commit
+        self._run_cmd('git checkout --orphan '+new_branch+' '+stop_sha)
+        self._run_cmd(['git', 'commit', '--author', self.phony_author, '-m', 'new root'])
 
-    name = benchmark.sections()[0]
-    isdir = os.path.isdir(name)
-    if not isdir:
-        print("QUITTING: "+name+" does not exist!")
-        exit()
+        # rebase the window of interest onto branch using the orphaned new_branch
+        # as a base
+        self._run_cmd('git rebase --onto '+new_branch+' '+stop_sha+' '+self.branch)
 
-    shutil.rmtree(name)
+        # get rid of the orphan branch
+        self._run_cmd('git branch -D '+new_branch)
 
-def read_benchmark_config(benchmark):
-    '''
-    Read a benchmark initialization file, and return the populated config object.
-    '''
-    config = configparser.ConfigParser()
-    config.read(benchmark)
-    return config
+    def _squash_unsynthesizable_commits(self):
+        '''
+        Squash the commits in the squash-list, into their oldest non-squash
+        list successor. The squash list is a list of commit indices to squash.
+        Must use indices instead of hashes since gfr rewrites hashes. For the
+        purposes of the squash-list we assume <branch> is index 0.
+        '''
+        new_branch = self.branch + '_new'
+        squash_shas = []
 
-def get_benchmarks(benchmark_dir):
+        # enusre we are starting at index 0
+        self._run_cmd('git checkout '+self.branch)
+
+        # get the hash of each commit in the squash list
+        for sidx in self.squash_list:
+            sha = self._run_cmd('git show -s HEAD~'+sidx+' --format=format:%H')[0]
+            squash_shas.append(sha)
+
+        # get hashes for all of the interesting commits
+        unsquashed = self._run_cmd('git log --format=format:%H')
+        unsquashed.reverse()
+
+        # create a new branch based off the root commit
+        self._run_cmd('git checkout '+unsquashed[0])
+        self._run_cmd('git checkout -b '+new_branch)
+
+        # cherry-pick each commit from the original branch onto the new branch
+        # for commits in the squash list, squash into the subsequent commit
+        to_squash = False
+        for cidx in range(1, len(unsquashed)):
+            self._run_cmd('git cherry-pick '+unsquashed[cidx])
+
+            # Does the last commit need to be squashed into the one just applied
+            if to_squash:
+                # squash the last two commits, and accept the default message
+                self._run_cmd('git reset --hard HEAD~2')
+                self._run_cmd('git merge --squash HEAD@{1}')
+                self._run_cmd(['git', 'commit', '--no-edit', '--author', self.phony_author])
+
+            # check if the commit we just applied needs to be squashed
+            if unsquashed[cidx] in squash_shas:
+                to_squash = True
+            else:
+                to_squash = False
+
+        # delete the unsquashed branch and rename the squashed branch
+        self._run_cmd('git branch -D '+self.branch)
+        self._run_cmd('git branch -m '+new_branch+' '+self.branch)
+
+
+def get_available_benchmarks(benchmark_dir):
     '''
     Check the `benchmark_dir` for .ini config files. Config files are assumed
     to be named the same as the source repo.
@@ -202,7 +233,7 @@ def main():
         description='build a timeseries HDL benchmark'
     )
 
-    benchmarks = get_benchmarks('benchmarks')
+    benchmarks = get_available_benchmarks('benchmarks')
     benchmark_names = benchmarks.keys()
 
     parser.add_argument('benchmark_name', choices=benchmark_names, help='build the named benchmark')
@@ -210,16 +241,11 @@ def main():
 
     args = parser.parse_args()
 
-    benchmark = read_benchmark_config(benchmarks[args.benchmark_name])
-
+    cbb = ChronbenchBenchmark(benchmarks[args.benchmark_name], 'git-filter-repo')
     if args.clean:
-        cleanup_benchmark(benchmark)
+        cbb.cleanup_benchmark()
     else:
-        clone_source_repo(benchmark)
-        reset_source_repo_head(benchmark)
-        domesticate_source_repo(benchmark, 'git-filter-repo')
-        truncate_filtered_repo(benchmark)
-        squash_uninteresting_commits(benchmark)
+        cbb.build_benchmark()
 
 if __name__ == '__main__':
     main()
