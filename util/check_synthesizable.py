1efe0eb716449f13eb99f4af6e0e78965db2c6aa
Zakary Nafziger <zsnafzig@ece.ubc.ca>
2024-11-08 16:10:49 -0800

    big refactor in check_synthesizable

--- util/check_synthesizable.py
+++ util/check_synthesizable.py
@@ -7,8 +7,155 @@ import configparser
 import math
 
 sys.path.insert(1, os.path.join('..'))
-from build_benchmark import get_benchmarks
-from build_benchmark import read_benchmark_config
+from build_benchmark import get_available_benchmarks
+from build_benchmark import ChronbenchBenchmark
+
+class SynthesisTool:
+    '''
+    A generic synthesis tool class
+    '''
+    def __init__(self, proj_dir, cbb):
+        self.proj_dir = proj_dir
+        self.cbb = cbb
+        pass
+
+    def run_synthesis(self):
+        print("Running Synthesis on: "+self.proj_dir)
+
+class VivadoSynthesis(SynthesisTool):
+    '''
+    Use vivado to synthesis a commit level synthesis project
+    '''
+
+    def run_synthesis(self):
+        print("Vivado Synthesis on: "+self.proj_dir)
+        self._build_synth_script()
+        self._write_synth_script()
+
+    def _build_synth_script(self):
+        '''
+        Create a tcl script to run Vivado synthesis
+        '''
+        # Check for any Vivado specific hacks
+        try:
+            vivado_extra_commands = self.cbb.benchmark['vivado-extra-commands'].split('\n')
+        except:
+            vivado_extra_commands = ''
+        try:
+            vivado_synth_args = self.cbb.benchmark['vivado-synth-args']
+        except:
+            vivado_synth_args = ''
+
+        # get the benchmark top module
+        top = self.cbb.benchmark['top']
+
+        # create the synth script
+        self.synth_script = [
+            'set outputdir autosynthxpr',
+            'set project autosynth',
+            'set partnumber xcvu3p-ffvc1517-3-e',
+            'file mkdir $outputdir',
+            'create_project -part $partnumber $project $outputdir',
+            'add_files src',
+            *vivado_extra_commands,
+            'set synth_args {'+vivado_synth_args+'}',
+            'catch {synth_design -top '+top+' {*}$synth_args}',
+            'exit',
+        ]
+
+    def _write_synth_script(self):
+        with open(os.path.join(self.proj_dir, 'vivado_synth_script.tcl'), 'w') as script:
+            for line in self.synth_script:
+                script.write(line+'\n')
+
+
+class CheckSynthesizable:
+    '''
+    Create commit level synthesis projects for each commit in the range
+    HEAD..HEAD~<depth> of the provided benchmark, and then run the provided
+    synthesis tool on each.
+    '''
+    def __init__(self, benchmark, depth, tool):
+        self.cbb = ChronbenchBenchmark(benchmark, None)
+        self.depth = depth
+        self.synth_dir = os.path.join('util', self.cbb.name + '_synth_projects')
+        self.tool = tool
+
+    def synthesize_snapshots(self):
+        '''
+        Create a synthesis project for each commit in the range
+        HEAD..HEAD~<depth>, and run synthesis in each.
+        '''
+        projects = self._setup_synthesis_projects()
+        for project in projects:
+            project.run_synthesis()
+
+    def _setup_synthesis_projects(self):
+            '''
+            Create a directory structure suitable for synthesizing many benchmark
+            commits in parallel.
+
+            Finished directory structure looks like this:
+            <benchmark>_synth_projects/
+                00_<sha>/
+                    src/
+                        <all source files from <benchmark>@<sha>>
+                01_<sha>/
+                    src/
+                        <all source files from <benchmark>@<sha>>
+                ...
+                depth_<sha>/
+                    src/
+                        <all source files from <benchmark>@<sha>>
+            '''
+            # create the main directory
+            self._create_experiment_dir()
+
+            # get a list of all available commits in the current benchmark
+            self.cbb._run_cmd('git checkout '+self.cbb.branch)
+            available_commits = self.cbb._run_cmd('git log --format=format:%H')
+ 
+            # figure out the number of commits to synthesize
+            depth = min(self.depth, int(self.cbb.depth))
+
+            # figure out how many digits to use in the prefix
+            prefix_digits = math.ceil(math.log(depth, 10))
+            prefix_str = "{:0"+str(prefix_digits)+"d}"
+
+            # starting at the most recent commit set up <depth> projects
+            projects = []
+            for cidx in range(depth):
+                prefix = prefix_str.format(cidx)
+                commit_dir = self._initialize_commit_dir(prefix, available_commits[cidx])
+                projects.append(self.tool(commit_dir, self.cbb))
+
+            return projects
+
+    def _create_experiment_dir(self):
+        '''
+        Create a directory to hold commit-level synthesis projects.
+        '''
+        if not os.path.isdir(self.synth_dir):
+            os.makedirs(self.synth_dir)
+        else:
+            print("QUITTING: A synthesis project for this benchmark already exists")
+            exit()
+
+    def _initialize_commit_dir(self, prefix, sha):
+        '''
+        Create a commit-level project directory, in the experiment directory.
+        Then checkout the benchmark at the corresponding commit and copy all
+        the sources into a `src` directory in the commit-level directory.
+
+        Returns the path to the commit-level directory
+        '''
+        commit_dir = os.path.join(self.synth_dir, prefix+'_'+sha)
+        os.makedirs(commit_dir)
+
+        self.cbb._run_cmd('git checkout '+sha)
+        shutil.copytree(self.cbb.name, os.path.join(commit_dir, 'src'), ignore=shutil.ignore_patterns('.git*'))
+
+        return commit_dir
 
 def run_vivado_synth(name, top, logdir, prefix, sha, vivado_extra_commands, vivado_synth_args):
     '''
@@ -84,46 +231,16 @@ def run_vivado_synth(name, top, logdir, prefix, sha, vivado_extra_commands, viva
 
     return success
 
-def get_sha(name):
-    '''
-    Get the SHA of HEAD in the repo `name`
+def main1():
+    os.chdir('..')
 
-    returns the SHA as a string
-    '''
-    sha = subprocess.run(['git', 'rev-parse', 'HEAD'], cwd=name, capture_output=True)
-    sha = sha.stdout.decode('utf8').replace('\n','')
-    return sha
+    benchmarks = get_available_benchmarks(os.path.join('benchmarks'))
 
-def checkout_sha(repo, sha):
-    msg = subprocess.run(['git', 'checkout', sha], cwd=repo, capture_output=True)
-    msg = msg.stderr.decode('utf8').replace('\n','n')
-    return msg
+    benchmark = benchmarks['cva5']
+    depth = 2
 
-def step_back_one_commit(name):
-    '''
-    Step HEAD back one commit in the repo `name`
-
-    returns false if end of history was detected, true otherwise
-    '''
-    # the message we expect if we hit the oldest commit
-    end_of_hist = "error: pathspec 'HEAD~1' did not match any file(s) known to git."
-
-    # attempt to step back one commit
-    msg = checkout_sha(name, 'HEAD~1')
-
-    # check to see if we hit oldest commit
-    if end_of_hist in msg:
-        return False
-    else:
-        return True
-
-def create_log_dir(logdir):
-    '''
-    If it doesn't all ready exist create a directory to preserve synthesis logs
-    '''
-    logdir_exists = os.path.isdir(logdir)
-    if not logdir_exists:
-        os.makedirs(logdir)
+    cs = CheckSynthesizable(benchmark, depth, VivadoSynthesis)
+    cs.synthesize_snapshots()
 
 def main():
     parser = argparse.ArgumentParser(
@@ -131,7 +248,7 @@ def main():
         description='attempt to synthesize each snapshot of a time series benchmark'
     )
 
-    benchmarks = get_benchmarks(os.path.join('..','benchmarks'))
+    benchmarks = get_available_benchmarks(os.path.join('..','benchmarks'))
     benchmark_names = benchmarks.keys()
 
     parser.add_argument('benchmark_name', choices=benchmark_names, help='synthesize the named benchmark')
@@ -181,4 +298,4 @@ def main():
             exit()
 
 if __name__ == '__main__':
-    main()
+    main1()
