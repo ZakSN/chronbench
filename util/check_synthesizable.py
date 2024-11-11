aa885b135d7dcf76790a4dde8ce6f21f38826e42
Zakary Nafziger <zsnafzig@ece.ubc.ca>
2024-11-11 13:28:44 -0800

    finish refactor of check_synthesizable.py

--- util/check_synthesizable.py
+++ util/check_synthesizable.py
@@ -24,13 +24,14 @@ class SynthesisTool:
 
 class VivadoSynthesis(SynthesisTool):
     '''
-    Use vivado to synthesis a commit level synthesis project
+    Use vivado to synthesize a commit level synthesis project
     '''
 
     def run_synthesis(self):
-        print("Vivado Synthesis on: "+self.proj_dir)
         self._build_synth_script()
         self._write_synth_script()
+        self._run_vivado()
+        self._report_result()
 
     def _build_synth_script(self):
         '''
@@ -64,10 +65,47 @@ class VivadoSynthesis(SynthesisTool):
         ]
 
     def _write_synth_script(self):
-        with open(os.path.join(self.proj_dir, 'vivado_synth_script.tcl'), 'w') as script:
+        self.synth_script_name = 'vivado_synth_script.tcl'
+        self.synth_script_path = os.path.join(self.proj_dir, self.synth_script_name)
+        with open(self.synth_script_path, 'w') as script:
             for line in self.synth_script:
                 script.write(line+'\n')
 
+    def _run_vivado(self):
+        '''
+        Run Vivado in headless mode to execute the synthscript in the commit
+        level project directory. Assume Vivado is on the path.
+        '''
+        subprocess.run(['vivado', '-mode', 'tcl', '-source', self.synth_script_name], cwd=self.proj_dir, capture_output=True)
+
+    def _report_result(self):
+        '''
+        Check to see if Vivado synthesis was successful. Prints message to
+        stdout, and writes a pass fail file in the project directory.
+        '''
+        # This message should turn up in the log to indicate successful synthesis
+        success_msg = 'synth_design completed successfully'
+
+        # Vivado logfiles are always called vivado.log
+        logfile = os.path.join(self.proj_dir, 'vivado.log')
+
+        # read the log to see if synthesis was successful
+        with open(logfile, 'r') as vlog:
+            synth_result = vlog.readlines()
+
+        success = False
+        for line in synth_result:
+            if success_msg in line:
+                success = True
+
+        def result(r):
+            with open(os.path.join(self.proj_dir, 'vivado_synth.'+r), 'w') as f:
+                f.write(r+'\n')
+            print(self.proj_dir+': '+r)
+        if success:
+            result('PASS')
+        else:
+            result('FAIL')
 
 class CheckSynthesizable:
     '''
@@ -114,7 +152,7 @@ class CheckSynthesizable:
             # get a list of all available commits in the current benchmark
             self.cbb._run_cmd('git checkout '+self.cbb.branch)
             available_commits = self.cbb._run_cmd('git log --format=format:%H')
- 
+
             # figure out the number of commits to synthesize
             depth = min(self.depth, int(self.cbb.depth))
 
@@ -157,145 +195,32 @@ class CheckSynthesizable:
 
         return commit_dir
 
-def run_vivado_synth(name, top, logdir, prefix, sha, vivado_extra_commands, vivado_synth_args):
-    '''
-    Synthesize the HDL located in name, with the top module as top, using
-    Vivado.
-
-    Assumes vivado is on the path, and uses `vivado_synth.tcl` to run
-    synthesis.
-
-    returns True if synthesis was successful
-    '''
-
-    # This message should turn up in the log to indicate successful synthesis
-    success_msg = 'synth_design completed successfully'
-
-    # The directories and files we expect Vivado to produce
-    projdir = 'autosynthxpr'
-    journalfile = 'vivado.jou'
-    logfile = 'vivado.log'
-    synth_script = 'vivado_synth.tcl'
-
-    # annoyingly vivado can't exec tcl from the command line, so we write a
-    # temporary script
-    with open(synth_script, 'w') as f:
-        f.writelines(
-            '''
-            set outputdir autosynthxpr
-            set project autosynth
-            set partnumber xcvu3p-ffvc1517-3-e
-
-            file mkdir $outputdir
-
-            create_project -part $partnumber $project $outputdir
-
-            add_files [lindex $argv 0]
-            '''
-            +vivado_extra_commands+
-            '''
-            set synth_args {'''+vivado_synth_args+'''}
-
-            catch {synth_design -top [lindex $argv 1] {*}$synth_args}
-
-            exit
-            ''')
-
-    # run vivado, and surpress the output
-    subprocess.run(['vivado', '-mode', 'tcl', '-source', synth_script, '-tclargs', name, top], capture_output=True)
-
-    # ensure that vivado ran properly and bailout other wise
-    xpr_exists = os.path.isdir(projdir)
-    journal_exists = os.path.isfile(journalfile)
-    log_exists = os.path.isfile(logfile)
-    if not (xpr_exists and journal_exists and log_exists):
-        print("FAILED: Seems like Vivado did not run.")
-        exit()
-
-    # read the log to see if synthesis was successful
-    with open(logfile, 'r') as vlog:
-        synth_result = vlog.readlines()
-
-    success = False
-    for line in synth_result:
-        if success_msg in line:
-            success = True
-
-    # clean up vivado project
-    shutil.rmtree(projdir)
-    os.remove(journalfile)
-    os.remove(synth_script)
-
-    # rename the log file with the result and sha
-    os.rename(logfile, os.path.join(logdir, prefix + "_" + sha + "_" + str(success) + ".log"))
-
-    return success
-
-def main1():
+def main():
     os.chdir('..')
 
-    benchmarks = get_available_benchmarks(os.path.join('benchmarks'))
-
-    benchmark = benchmarks['cva5']
-    depth = 2
-
-    cs = CheckSynthesizable(benchmark, depth, VivadoSynthesis)
-    cs.synthesize_snapshots()
-
-def main():
     parser = argparse.ArgumentParser(
         prog='check_synthesizable.py',
         description='attempt to synthesize each snapshot of a time series benchmark'
     )
 
-    benchmarks = get_available_benchmarks(os.path.join('..','benchmarks'))
+    benchmarks = get_available_benchmarks('benchmarks')
     benchmark_names = benchmarks.keys()
 
+    tools = {
+        'vivado': VivadoSynthesis
+    }
+
+    parser.add_argument('tool', choices=tools.keys(), help='synthesis tool to use')
     parser.add_argument('benchmark_name', choices=benchmark_names, help='synthesize the named benchmark')
-    parser.add_argument('depth', type=int, help='number of predecessor commit to try synthesizing')
+    parser.add_argument('depth', type=int, help='number of predecessor commits to try synthesizing')
 
     args = parser.parse_args()
 
-    benchmark = read_benchmark_config(benchmarks[args.benchmark_name])
+    benchmark = benchmarks[args.benchmark_name]
+    depth = args.depth
 
-    bpath = os.path.join('..',args.benchmark_name)
-    top = benchmark[args.benchmark_name]['top']
-    start_sha = benchmark[args.benchmark_name]['branch']
-
-    #populate optional fields
-    try:
-        vivado_extra_commands = benchmark[args.benchmark_name]['vivado-extra-commands']
-    except:
-        vivado_extra_commands = ''
-    try:
-        vivado_synth_args = benchmark[args.benchmark_name]['vivado-synth-args']
-    except:
-        vivado_synth_args = ''
-
-    commit_depth = args.depth
-
-    # make sure we're starting at the right spot
-    checkout_sha(bpath, start_sha)
-
-    logdir = "synth_logs"
-    create_log_dir(logdir)
-    prefix_digits = math.ceil(math.log(commit_depth, 10))
-
-    for prefix in range(commit_depth):
-        sha = get_sha(bpath)
-        prefix_str = "{:0"+str(prefix_digits)+"d}"
-        prefix_str = prefix_str.format(prefix)
-        synth_result = run_vivado_synth(bpath, top, logdir, prefix_str, sha, vivado_extra_commands, vivado_synth_args)
-
-        if synth_result:
-            print(sha + " Synthesizable")
-        else:
-            print(sha + " Unsynthesizable")
-
-        stepped = step_back_one_commit(bpath)
-        if not stepped:
-            print("QUITTING: reached end of history")
-            exit()
+    cs = CheckSynthesizable(benchmark, depth, tools[args.tool])
+    cs.synthesize_snapshots()
 
 if __name__ == '__main__':
-    main1()
+    main()
