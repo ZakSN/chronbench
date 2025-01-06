73714b3479c33d9ce081778a06bfa514ba9447d9
Zakary Nafziger <zsnafzig@ece.ubc.ca>
2025-01-06 15:56:38 -0800

    fix bug in how we were counting HDL change

--- util/plot_delta.py
+++ util/plot_delta.py
@@ -6,66 +6,71 @@ import numpy as np
 
 from plot_qor import collect_tmin_data
 from plot_qor import collect_util_data
-from plot_stats import read_stats_files
 from characterize_benchmark import SetupCharacterizationProjects
 sys.path.insert(1, os.path.join('..'))
 from build_benchmark import get_available_benchmarks
 
+def get_src_stats(cbb):
+    '''
+    Count the number of lines impacted by each commit in ChronBench Benchmark
+    `cbb'
+    '''
+    cbb._run_cmd('git checkout '+cbb.branch)
+    raw_src_stats = cbb._run_cmd('git log --shortstat --format=format:%at')
+    src_stats = []
+    for line in raw_src_stats:
+        try:
+            int(line)
+        except ValueError:
+            if len(line) != 0:
+                net = 0
+                for change in line.split(',')[1:]:
+                    if '+' in change:
+                        net = net + int(change.split()[0])
+                    else:
+                        net = net + int(change.split()[0])
+                src_stats.append(net)
+    return src_stats
+
+def repackage_data(data):
+    '''
+    repackage the raw src, utilization, and timing statistics into a coherent
+    dictionary:
+        key: benchmark name
+        value: [<delta_sloc, clbs, fmid, frange>...]
+    '''
+    repack_data = dict.fromkeys(data)
+    for benchmark_name, raw in data.items():
+        commits = []
+        for cidx in range(len(raw[0])):
+            delta_sloc = raw[0][cidx]
+            # util
+            clbs = raw[1][1][cidx]
+            # tmin
+            fmid = raw[2][1][cidx]
+            frange = raw[2][2][cidx]
+            commits.append((delta_sloc, clbs, fmid, frange))
+        repack_data[benchmark_name] = commits
+    return repack_data
+
 def reduce_data(data):
-    for benchmark_name, benchmark_data in data.items():
-        # reduce to interesting commits
-        commit_data = benchmark_data[0]
-        interesting_commit_data = []
-        for commit in commit_data:
-            if commit[2] == 2:
-                interesting_commit_data.append(commit)
-        data[benchmark_name][0] = list(reversed(interesting_commit_data))
-
-        # repackage everything into a more useful format
-        # TODO: this is pretty ugly, but basically we have to read the squash
-        # to squash stats from interesting commits to align them with QoR data
-        # better way to do this may be to read directly from a benchmark repo
-        reduced = []
-        squash_list = data[benchmark_name][1]
-        synth_idx = 0
-        for real_idx in range(len(data[benchmark_name][0])+1):
-            try:
-                ts = data[benchmark_name][0][real_idx][0]
-                deltasloc = data[benchmark_name][0][real_idx][1]
-            except IndexError:
-                ts = None
-                deltasloc = None
-            if real_idx not in squash_list:
-                print('adding '+str(real_idx)+' as '+str(synth_idx))
-                util = data[benchmark_name][2][1][synth_idx]
-                fmid = data[benchmark_name][3][1][synth_idx]
-                frange = data[benchmark_name][3][2][synth_idx]
-                reduced.append((ts, deltasloc, util, fmid, frange, False))
-                synth_idx += 1
-            else:
-                print('squashing '+str(real_idx)+' into '+str(len(reduced)-1)+' ('+str(synth_idx)+')')
-                squash = reduced[-1]
-                squash = (squash[0], squash[1] + deltasloc, squash[2], squash[3], squash[4], True)
-                reduced[-1] = squash
-        data[benchmark_name] = reduced
-    return data
-
-def compute_qor_deltas(data):
-    deltas = dict.fromkeys(data)
-    for benchmark_name, benchmark_data in data.items():
-        qor_delta = []
-        for idx in range(len(benchmark_data) - 1):
-            N  = benchmark_data[idx]
-            dN = benchmark_data[idx+1]
-            qor_delta.append((
-                N[0],
-                N[1],
-                abs(N[2] - dN[2]),
-                abs(N[3] - dN[3]),
-                abs(N[4] - dN[4])
-            ))
-        deltas[benchmark_name] = qor_delta
-    return deltas
+    '''
+    reduce the repackaged data to compute delta clbs, delta fmid, and delta frange
+    we leave delta_sloc as is reported by git. Consider the delta_sloc in the
+    root commit to be 0, since this commit simply adds all of the files at the
+    beginning of the WoI
+    '''
+    reduced_data = dict.fromkeys(data)
+    for benchmark_name, commits in data.items():
+        deltas = []
+        for cidx in range(len(commits)-1):
+            delta_sloc = commits[cidx][0]
+            delta_clbs = abs(commits[cidx][1] - commits[cidx+1][1])
+            delta_fmid = abs(commits[cidx][2] - commits[cidx+1][2])
+            delta_frange = abs(commits[cidx][3] - commits[cidx+1][3])
+            deltas.append((delta_sloc, delta_clbs, delta_fmid, delta_frange))
+        reduced_data[benchmark_name] = deltas
+    return reduced_data
 
 def plot_sloc_vs_hw(dsloc, dhw):
     plt.scatter(dsloc, dhw, marker='.')
@@ -124,9 +129,6 @@ def main():
             'jt12' : None,
             'vortex' : None,
            }
-
-    benchmark_names = data.keys()
-
     for benchmark_name in benchmark_names:
         char_proj = SetupCharacterizationProjects(benchmarks[benchmark_name], tool)
         projs = char_proj.build_directory_structure()
@@ -134,40 +136,28 @@ def main():
         tmin_data = collect_tmin_data(projs[1])
         util_data = collect_util_data(projs[1])
 
-        squash_list = char_proj.cbb.squash_list
-        squash_list = [int(s) for s in squash_list]
+        src_stats = get_src_stats(char_proj.cbb)
+        data[benchmark_name] = (src_stats, util_data, tmin_data)
 
-        data[benchmark_name] = (squash_list, util_data, tmin_data)
-
-    os.chdir('util')
-    commit_data, _ = read_stats_files(benchmark_names)
-    os.chdir('..')
-
-    for idx, benchmark_name in enumerate(benchmark_names):
-        data[benchmark_name] = [commit_data[idx], *data[benchmark_name]]
-
+    data = repackage_data(data)
     data = reduce_data(data)
-    deltas = compute_qor_deltas(data)
-
-    def print_data_dict(data):
-        for benchmark in data.keys():
-            print(benchmark)
-            for idx, item in enumerate(data[benchmark]):
-                item = [str(i) for i in item]
-                print('\t'+str(idx)+' '+' '.join(item))
-    print_data_dict(data)
-    print_data_dict(deltas)
+    for benchmark in data.keys():
+        print(benchmark)
+        for idx, item in enumerate(data[benchmark]):
+            item = [str(i) for i in item]
+            print('\t'+str(idx)+' '+' '.join(item))
 
     dsloc = []
     dhw = []
-    for benchmark_name, data in deltas.items():
-        dsloc += [s[1] for s in data]
-        dhw += [h[2] for h in data]
+    for benchmark_name, deltas in data.items():
+        dsloc += [s[0] for s in deltas]
+        dhw += [h[1] for h in deltas]
     dsloc = np.array(dsloc)
     dhw = np.array(dhw)
 
     plot_sloc_vs_hw(dsloc, dhw)
     plot_hw_hist(dhw)
+    
 
 if __name__ == '__main__':
     main()
